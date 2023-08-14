4d8e7acb37712cd9c77b8f95ab03ff3dd4bff52c
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2023-08-14 20:20:00 +0500

    Update home_screen.dart

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:currency_convertor/Model/rates_model.dart';
+import 'package:currency_convertor/View/Widgets/conversion_card.dart';
+import 'package:currency_convertor/ViewModel/currencies_view_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,13 +11,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<RatesModel> ratesModel;
+  late Future<Map> currenciesModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    ratesModel = fetchRates();
+    currenciesModel = fetchCurrencies();
+  }
+
+  Widget _buildConversionCard(Map rates, Map currencies) {
+    return ConversionCard(
+      rates: rates,
+      currencies: currencies,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Currency Convertor'),
       ),
-      body: Column(),
+      body: FutureBuilder<RatesModel>(
+        future: ratesModel,
+        builder: (context, snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          } else {
+            return FutureBuilder<Map>(
+                future: currenciesModel,
+                builder: (context, index){
+                  if (index.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator(),);
+                  } else if (index.hasError) {
+                    return Center(child: Text('Error: ${index.error}'));
+                  } else {
+                    return _buildConversionCard(snapshot.data!.rates, index.data!);
+                  }
+                }
+            );
+          }
+        }
+      ),
     );
   }
 }
