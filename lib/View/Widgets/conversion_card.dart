40660a80d54d43d499ece88384e45df12c5cb1ca
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2023-08-14 20:20:12 +0500

    Create conversion_card.dart

---
+++ lib/View/Widgets/conversion_card.dart
@@ -0,0 +1,112 @@
+import 'package:currency_convertor/View/Widgets/dropdown_row.dart';
+import 'package:flutter/material.dart';
+
+class ConversionCard extends StatefulWidget {
+  final rates;
+  final Map currencies;
+  const ConversionCard({super.key, @required this.rates, required this.currencies});
+
+  @override
+  State<ConversionCard> createState() => _ConversionCardState();
+}
+
+class _ConversionCardState extends State<ConversionCard> {
+  TextEditingController amountController = TextEditingController();
+  String dropdownValue1 = 'USD';
+  String dropdownValue2 = 'PKR';
+  String conversion = '';
+  bool isLoading = false;
+
+  void startLoading() {
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  void stopLoading() {
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  String convert(
+      Map exchangeRates,
+      String amount,
+      String currencyBase,
+      String currencyFinal,
+      ) {
+    double usdAmount = double.parse(amount) / exchangeRates[currencyBase];
+    String output =
+    (usdAmount * exchangeRates[currencyFinal]).toStringAsFixed(2);
+    return output;
+  }
+
+  void convertAndDisplay() {
+    conversion =
+    '${amountController.text} $dropdownValue1 = ${convert(widget.rates, amountController.text, dropdownValue1, dropdownValue2)} $dropdownValue2';
+    stopLoading();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextFormField(
+            controller: amountController,
+            decoration: const InputDecoration(
+              hintText: 'Enter Amount',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          const SizedBox(height: 20,),
+          DropdownRow(
+            label: 'From:',
+            value: dropdownValue1,
+            currencies: widget.currencies,
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue1 = newValue!;
+              });
+            },
+          ),
+          const SizedBox(height: 20,),
+          DropdownRow(
+            label: 'To:',
+            value: dropdownValue2,
+            currencies: widget.currencies,
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue1 = newValue!;
+              });
+            },
+          ),
+          const SizedBox(height: 20,),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    startLoading();
+                    conversion = '';
+                    convertAndDisplay();
+                  },
+                  child: isLoading
+                      ? const CircularProgressIndicator()
+                      : const Text('Convert'),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20,),
+          Text(
+            conversion,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
